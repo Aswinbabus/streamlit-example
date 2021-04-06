7f4a1fae1a7a653823c738d675ca5426407e7a1e
ASWIN BABU S <44750796+Aswinbabus@users.noreply.github.com>
2021-04-06 20:51:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ st.sidebar.write("Click here to open Loan Prediction Application")
 image = Image.open('loan.jpg')
 st.image(image)
 if st.sidebar.button('Predict Loan'):
-    js = "window.open('https://www.streamlit.io/')"  
-    js = "window.location.href = 'https://www.streamlit.io/'"  
+    js = "window.open('https://loan-prediction-app-ai.herokuapp.com/')"  
+    js = "window.location.href = 'https://loan-prediction-app-ai.herokuapp.com/'"  
     html = '<img src onerror="{}">'.format(js)
     div = Div(text=html)
     st.bokeh_chart(div)
